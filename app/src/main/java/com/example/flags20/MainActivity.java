7c3207e24e5e8b83a786d0264525beea5a9b46af
dz2244 <dz2244@bs.amalnet.k12.il>
2024-12-13 08:17:47 +0200

    commit1

--- app/src/main/java/com/example/flags20/MainActivity.java
+++ app/src/main/java/com/example/flags20/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.flags20;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener{
+    Spinner spinner;
+    int[]images = {R.drawable.}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<Integer> adpS = new ArrayAdapter<Integer>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item ,images);
+        spinner.setAdapter(adpS);
+
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
